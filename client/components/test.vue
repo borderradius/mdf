02cb7559c806172949c486e384b84115138b6333
N-0003 <N-0003@N-0003ui-MacBook-Pro.local>
2019-12-06 18:12:26 +0900

    first commit

---
+++ client/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    this is test components
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class Test extends Vue {}
+</script>
+
+<style scoped></style>
---
+++ client/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <test />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import test from '../components/test.vue';
+
+@Component({
+  components: {
+    test,
+  },
+})
+export default class extends Vue {
+  title: string = 'Hello world!';
+}
+</script>
