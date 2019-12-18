3c8111e5f3d1789254b1836a2ad8fcbdc95194eb
N-0003 <N-0003@N-0003ui-MacBook-Pro.local>
2019-12-18 10:30:00 +0900

    start 및 new badge 추가

--- client/components/test.vue
+++ client/components/test.vue
@@ -2,22 +2,38 @@
   <div class="bg-white border rounded-lg w-1/4 overflow-hidden">
     <img :src="houseInfo.imageUrl" :alt="houseInfo.imageAlt" />
     <div class="p-6">
-      <div class="text-gray-600 text-sm uppercase font-semibold tracking-wide">
-        {{ houseInfo.beds }} beds &bull; {{ houseInfo.baths }} baths
+      <div class="flex items-center">
+        <span
+          class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full font-semibold uppercase tracking-wide"
+          >New</span
+        >
+        <div
+          class="ml-2 text-gray-600 text-sm uppercase font-semibold tracking-wide"
+        >
+          {{ houseInfo.beds }} beds &bull; {{ houseInfo.baths }} baths
+        </div>
       </div>
-      <h4 class="font-semibold text-lg leading-tight truncate">
+      <h4 class="mt-1 font-semibold text-lg leading-tight truncate">
         {{ houseInfo.title }}
       </h4>
       <div class="mt-1">
         {{ houseInfo.formattedPrice }}
         <span class="text-gray-600 text-sm">/ wk</span>
       </div>
-      <div class="mt-4">
-        <span class="text-teal-600 font-semibold"
-          >{{ houseInfo.rating }}/5 start</span
+      <div class="mt-2 flex items-center">
+        <svg
+          v-for="i in 5"
+          :key="i"
+          :class="i <= property.rating ? 'text-teal-400' : 'text-gray-300'"
+          class="h-4 w-4 fill-current"
+          viewBox="0 0 20 20"
         >
-        <span class="text-gray-600 text-sm"
-          >(based on {{ houseInfo.reviewCount }} reviews)</span
+          <path
+            d="M10,1.3l2.388,6.722H18.8l-5.232,3.948l1.871,6.928L10,14.744l-5.438,4.154l1.87-6.928L1.199,8.022h6.412L10,1.3z"
+          />
+        </svg>
+        <span class="ml-2 text-gray-600 text-sm"
+          >{{ houseInfo.reviewCount }} reviews</span
         >
       </div>
     </div>
